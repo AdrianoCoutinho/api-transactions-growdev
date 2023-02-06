7cc0c6ead966fb8690de0bed997b93e2eca9f3e1
AdrianoCoutinho <adriano.98@outlook.com>
2023-02-06 20:48:05 -0300

    ajustes para rodar no render

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
 import { userRoutes } from "./routes/user.routes";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", userRoutes());
 
